8930868e6e52f55c44a1e35183353933c0e82944
Djordje Stankovic <djordjestankovic87@gmail.com>
2022-10-11 22:20:30 +0200

    test fix 9

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+//app.use("/api/product", product);
+
+app.get('/home',(req,res)=>{
+    res.send("Hello world");
+});
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
