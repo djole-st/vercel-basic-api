d0ba758ff6db5cd235daea8a71b0b3a61ddf9bf2
Djordje Stankovic <djordjestankovic87@gmail.com>
2022-10-11 21:55:04 +0200

    fixnew6

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use('/home',(req,res)=>{
+app.get('/',(req,res)=>{
+    res.send("Hello from base!");
+});
+
+app.get('/home',(req,res)=>{
     res.send("Hello world");
 });
 
 app.listen(PORT,()=>{
     console.log(`Server started at port ${PORT}`);
 });
+
+module.exports = app;
