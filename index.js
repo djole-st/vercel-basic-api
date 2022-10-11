2afbfee1ddfd90f60fefb9b7c8d2a1e89b8eef0f
Djordje Stankovic <djordjestankovic87@gmail.com>
2022-10-11 21:49:20 +0200

    fixnew5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use((req,res)=>{
+app.use('/home',(req,res)=>{
     res.send("Hello world");
 });
 
